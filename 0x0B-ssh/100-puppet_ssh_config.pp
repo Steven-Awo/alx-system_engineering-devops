0f16e336e802514778d90739485030240c12c6ba
StevenAwo <rotimiawomodu@gmail.com>
2024-01-01 22:18:12 +0100

    task 4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
-#Using puppet to configure file
+# Using puppet to configure file
 
-file { 'etc/ssh/ssh_config':
-	ensure => present,
-
-content =>"
-
-	#SSH client conffiguration
-	host*
-	IdentifyFile ~/.ssh/school
-	PasswordAuthentication no
+file { '/etc/ssh/ssh_config':
+  ensure  => present,
+  content => "# SSH client configuration
+  Host *
+    IdentityFile ~/.ssh/school
+    PasswordAuthentication no
+  ",
 }
