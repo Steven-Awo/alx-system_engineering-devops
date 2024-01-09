7d80f5967c68c69ea814991a784f4dc410bacdb4
StevenAwo <rotimiawomodu@gmail.com>
2024-01-09 01:01:12 +0100

    TASK 2

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,20 @@
+
+# Installing tthe Nginx's package
+package { 'nginx':
+  ensure => installed,
+
+}
+
+file { '/etc/nginx/conf.d/custom_headers.conf':
+  ensure  => present,
+  content => 'add_header X-Served-By $hostname;',
+  require => Package['nginx'],
+
+}
+
+service { 'nginx':
+  ensure    => running,
+  enable    => true,
+  subscribe => File['/etc/nginx/conf.d/custom_headers.conf'],
+
+}
