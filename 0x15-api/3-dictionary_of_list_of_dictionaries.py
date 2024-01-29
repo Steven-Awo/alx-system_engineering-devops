182a8c28c332038d3e25930d4c4250021f4b940b
StevenAwo <rotimiawomodu@gmail.com>
2024-01-29 15:22:59 +0100

    TASK 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-A python scrip that uses what you did in the task #0,
+A python script that uses what you did in the task #0,
 extend your Python script to export data in the JSON format.
 """
 
