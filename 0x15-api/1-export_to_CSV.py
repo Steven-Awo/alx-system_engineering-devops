2afaf437550bc0b7e7357ea05965f8ef332c7f0c
StevenAwo <rotimiawomodu@gmail.com>
2024-01-29 13:31:14 +0100

    TASK 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,46 +1,39 @@
 #!/usr/bin/python3
 """
-Write a Python script that, using what you did in the task
-#0, extend your Python script to export data in
-the CSV format.
+A Script that, uses a REST API, for a given employee ID, returns
+information about his/her TODO list progress
+exporting data in the CSV format.
 """
 
-import requests
-
 import csv
-
+import json
+import requests
 from sys import argv
+
 
 if __name__ == "__main__":
 
     sessionReq = requests.Session()
 
-    Emplye_id = argv[1]
-    URL_id = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(Emplye_id)
-    name_of_URL = 'https://jsonplaceholder.typicode.com/users/{}'.format(Emplye_id)
+    idEmp = argv[1]
+    idURL = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(idEmp)
+    nameURL = 'https://jsonplaceholder.typicode.com/users/{}'.format(idEmp)
 
-    emplye = sessionReq.get(URL_id)
-    name_of_emplye = sessionReq.get(name_of_URL)
+    employee = sessionReq.get(idURL)
+    employeeName = sessionReq.get(nameURL)
 
-    json_requts = emplye.json()
-    name = name_of_emplye.json()['name']
+    json_req = employee.json()
+    usr = employeeName.json()['username']
 
-    total_numb_of_tasks = 0
+    totalTasks = 0
 
-    for tasks_that_has_been_done in json_requts:
-        if tasks_that_has_been_done['completed']:
-            total_numb_of_tasks += 1
+    for done_tasks in json_req:
+        if done_tasks['completed']:
+            totalTasks += 1
 
-    filee_CSV = Emplye_id + '.csv'
+    fileCSV = idEmp + '.csv'
 
-    with open(filee_CSV, "w", newline='') as csv_file:
-        write = csv.writer(csv_file, delimiter=',', quoting=csv.QUOTE_ALL)
-        for x in json_requts:
-            write.writerow([Emplye_id, usr, x.get('completed'), x.get('title')])
-
-    # print("Employee {} is done with tasks({}/{}):".
-    #       format(name, total_numb_of_tasks, len(json_requts)))
-
-    # for tasks_that_has_been_done in json_requts:
-    #     if tasks_that_has_been_done['completed']:
-    #         print("\t " + tasks_that_has_been_done.get('title'))
+    with open(fileCSV, "w", newline='') as csvfile:
+        write = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_ALL)
+        for i in json_req:
+            write.writerow([idEmp, usr, i.get('completed'), i.get('title')])
