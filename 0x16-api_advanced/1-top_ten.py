ebe63d21d64a2590ae4a0bd1ed0527774679c5d8
StevenAwo <rotimiawomodu@gmail.com>
2024-02-13 20:45:41 +0100

    Task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
-"""Function to print hot posts on a given Reddit subreddit."""
+"""Function thats to print the hot posts on the given Reddit
+subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
+    """Prints out the titles thats of the 10 most hottest
+    posts on the a given subreddit."""
+    urll = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headders = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    params = {
+    parrams = {
         "limit": 10
     }
-    response = requests.get(url, headers=headers, params=params,
+    respse = requests.get(urll, headders=headders, parrams=parrams,
                             allow_redirects=False)
-    if response.status_code == 404:
+    if respse.status_code == 404:
         print("None")
         return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+    resullts = respse.json().get("data")
+    [print(c.get("data").get("title")) for c in resullts.get("children")]
