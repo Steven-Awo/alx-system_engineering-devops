a91832fff95d4000bedf77b87e8c645935243107
StevenAwo <rotimiawomodu@gmail.com>
2024-02-13 20:05:17 +0100

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,10 +8,10 @@ def number_of_subscribers(subreddit):
     """Returning the total number of the subscribers on that given
     subreddit."""
     urll = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headders = {
+    headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    respn = requests.get(urll, headders=headders, allow_redirects=False)
+    respn = requests.get(urll, headers=headers, allow_redirects=False)
     if respn.status_code == 404:
         return 0
     resullts = respn.json().get("data")
