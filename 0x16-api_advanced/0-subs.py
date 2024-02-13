ae398280ab505234851a995f34154e278920baf7
StevenAwo <rotimiawomodu@gmail.com>
2024-02-13 20:10:57 +0100

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-"""Function thats to the query of subscribers on a particular
-given in Reddit subreddit."""
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returning the total number of the subscribers on that given
-    subreddit."""
+    """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
