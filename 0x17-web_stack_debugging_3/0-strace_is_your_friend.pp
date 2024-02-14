209383202aa57f5155319a76544366bb94854d54
StevenAwo <rotimiawomodu@gmail.com>
2024-02-14 21:15:11 +0100

    Task 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,10 +1,10 @@
 # A puppet's manuscript thats to replace just a line in the particular file thats on a server
 
-$file_to_edit = '/var/www/html/wp-settings.php'
+$file_to_editt = '/var/www/html/wp-settings.php'
 
 #replacing the line that containing "phpp" with "php"
 
 exec { 'replace_line':
-  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
+  command => "sed -i 's/phpp/php/g' ${file_to_editt}",
   path    => ['/bin','/usr/bin']
 }
