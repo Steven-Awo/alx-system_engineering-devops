6b7ab4ac80aa5a3566a924605b7c5763f68bf12d
StevenAwo <rotimiawomodu@gmail.com>
2024-02-14 21:12:07 +0100

    Task 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,7 +2,7 @@
 
 $file_to_edit = '/var/www/html/wp-settings.php'
 
-#replace line containing "phpp" with "php"
+#replacing the line that containing "phpp" with "php"
 
 exec { 'replace_line':
   command => "sed -i 's/phpp/php/g' ${file_to_edit}",
