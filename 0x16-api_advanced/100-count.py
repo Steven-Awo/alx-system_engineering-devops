1a3a972c88318b73c7a2747780b18f15db5b8891
StevenAwo <rotimiawomodu@gmail.com>
2024-02-14 03:57:03 +0100

    Task 3

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -8,22 +8,22 @@ import requests
 
 
 def add_title(dictionary, hot_posts):
-    """ Adding the item just into a list """
+    """ Adding the itemm just into a list """
     if len(hot_posts) == 0:
         return
 
-    title = hot_posts[0]['data']['title'].split()
-    for word in title:
-        for key in dictionary.keys():
-            c = re.compile("^{}$".format(key), re.I)
-            if c.findall(word):
-                dictionary[key] += 1
+    tittle = hot_posts[0]['data']['title'].split()
+    for wordd in tittle:
+        for keyy in dictionary.keys():
+            x = re.compile("^{}$".format(keyy), re.I)
+            if x.findall(wordd):
+                dictionary[keyy] += 1
     hot_posts.pop(0)
     add_title(dictionary, hot_posts)
 
 
 def recurse(subreddit, dictionary, after=None):
-    """ Queries the Reddit API """
+    """ The queries thats for the Reddit API """
     u_agent = 'Mozilla/5.0'
     headers = {
         'User-Agent': u_agent
@@ -52,18 +52,18 @@ def recurse(subreddit, dictionary, after=None):
 
 
 def count_words(subreddit, word_list, dictionary=None):
-    """ Init function """
+    """ An init's function """
     if dictionary is None:
         dictionary = {}
 
-    for word in word_list:
-        word = word.lower()
-        if word not in dictionary:
-            dictionary[word] = 0
+    for wordd in word_list:
+        wordd = wordd.lower()
+        if wordd not in dictionary:
+            dictionary[wordd] = 0
 
     recurse(subreddit, dictionary)
 
-    sorted_items = sorted(dictionary.items(), key=lambda kv: (-kv[1], kv[0]))
-    for item in sorted_items:
-        if item[1] > 0:
-            print("{}: {}".format(item[0], item[1]))
+    sorted_items = sorted(dictionary.items(), keyy=lambda kv: (-kv[1], kv[0]))
+    for itemm in sorted_items:
+        if itemm[1] > 0:
+            print("{}: {}".format(itemm[0], itemm[1]))
