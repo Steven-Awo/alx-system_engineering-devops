e54c70361c10ac38ce0c7f678b3e5eef642ade17
StevenAwo <rotimiawomodu@gmail.com>
2024-02-19 17:48:48 +0100

    TASK 1

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,14 @@
+# This particular manuscript actually enables the user in the holberton to be able to login and open the files without any error
+
+# Increasing the hard file's limit for the user holberton
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+
+# Increasing the soft file's limit for the user called holberton
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
